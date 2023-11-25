93e0371e0ee030086dd901c2d3ca5306b97a6738
Jesus Hernandez <jesus.hernandez@sicuentame.com>
2023-11-25 01:08:56 -0600

    mobile first

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,7 @@ const App = () => {
 
         <section className="container mx-auto mt-8  px-4">
           <div className="flex  justify-center gap-4 rounded-md bg-white p-4">
-            <button className="text-blue-600">All</button>
+            <button className="text-blue-600">All list</button>
             <button className="hover:text-blue-600">Active</button>
             <button className="hover:text-blue-600">Completed</button>
           </div>
